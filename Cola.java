a39a2ddb60b930dbf4e00654ff2a006976074c64
LAURA CATALINA PRECIADO BALLEN <92498586+kt6delta@users.noreply.github.com>
2022-07-07 09:02:33 +0000

    uniendo gui y logic

--- Cola.java
+++ Cola.java
@@ -1,19 +1,65 @@
 import java.util.Scanner;
 public class Cola{
   Scanner sc = new Scanner(System.in);
-  Nodo first;
-  Nodo last;
+  private Nodo first;
+  private Nodo last;
   private int num_deal=4;
+  private int idClient;
+  private int deal;
+  int lugar=1;
+  GUI n = new GUI();
   
   public Cola(){
     first= null;
     last= null;
+    n.setVisible(true);
+    for(int i=0; i<9; i++){
+    }
   }
   
   public void Insert(int id_client){
     Nodo new1= new Nodo();
     new1.deal = (int)(Math.random()*8+1); //entre 1- 8 transacciones
     new1.id_client=id_client;
+    new1.posicion=id_client;
+    switch(posicion){
+      case 1:
+        n.id1.setText(String.valueOf(new1.id_client));
+        n.deal1.setText(String.valueOf(new1.deal));
+        break;
+      case 2:
+        n.id2.setText(String.valueOf(new1.id_client));
+        n.deal2.setText(String.valueOf(new1.deal));
+        break;
+      case 3:
+        n.id3.setText(String.valueOf(new1.id_client));
+        n.deal3.setText(String.valueOf(new1.deal));
+        break;
+      case 4:
+        n.id4.setText(String.valueOf(new1.id_client));
+        n.deal4.setText(String.valueOf(new1.deal));
+        break;
+      case 5:
+        n.id5.setText(String.valueOf(new1.id_client));
+        n.deal5.setText(String.valueOf(new1.deal));
+        break;
+      case 6:
+        n.id6.setText(String.valueOf(new1.id_client));
+        n.deal6.setText(String.valueOf(new1.deal));
+        break;
+      case 7:
+        n.id7.setText(String.valueOf(new1.id_client));
+        n.deal7.setText(String.valueOf(new1.deal));
+        break;
+      case 8:
+        n.id8.setText(String.valueOf(new1.id_client));
+        n.deal8.setText(String.valueOf(new1.deal));
+        break;
+      case 9:
+        n.id9.setText(String.valueOf(new1.id_client));
+        n.deal9.setText(String.valueOf(new1.deal));
+        break;
+    }
     if(first == null){
       first= new1;
       last=new1;
@@ -31,34 +77,9 @@ public class Cola{
     Actual = first;
     if(first != null){
       do{
-        System.out.println(Actual.id_client +" "+ Actual.deal);
+        System.out.println(Actual.id_client+" "+Actual.deal);
         Actual= Actual.next;
       }while(Actual != first);
-    }else{
-      System.out.println("Lista vacia");
-    }
-  }
-
-  public void Serch(){
-    Nodo Actual= new Nodo();
-    Actual = first;
-    boolean find = false;
-    
-    System.out.println("Ingrese el dato del nodo a buscar:");
-    int serch= sc.nextInt();
-    
-    if(first != null){
-      do{
-        if(Actual.id_client == serch){
-         System.out.println("\n nodo encontrado");
-          find = true;
-        }
-        System.out.println(Actual.id_client);
-        Actual= Actual.next;
-      }while(Actual != first && find != true);
-      if(!find){
-        System.out.println("No encontrado");
-      }
     }else{
       System.out.println("Lista vacia");
     }
@@ -90,14 +111,11 @@ public class Cola{
     Actual = first;
     Nodo Anterior = new Nodo();
     Anterior =null;
-    boolean find = false;
-
-    System.out.println("Ingrese el dato del nodo eliminar:");
-    int serch= sc.nextInt();
-
+    boolean find=false;
+    
     if(first != null){
       do{
-        if(Actual.id_client == serch){
+        if(Actual.deal <= num_deal){
          System.out.println("\n nodo encontrado");
           if(Actual== first){
             first= first.next;
@@ -111,15 +129,28 @@ public class Cola{
           System.out.println("nodo Eliminado");
           find = true;
         }
-        
         Anterior= Actual;
         Actual=Actual.next;
-      }while(Actual != first && find != true);
+      }while(Actual!= last);
+      
       if(!find){
         System.out.println("No encontrado");
       }
     }else{
       System.out.println("Lista vacia");
     }
+  }
+  
+  public void SetIdClient(int idClient){
+    this.idClient=idClient;
+  }
+  public int GetidClient(){
+    return idClient;
+  }
+  public void Setdeal(int deal){
+    this.deal=deal;
+  }
+  public int Getdeal(){
+    return deal;
   }
 }
--- Main.java
+++ Main.java
@@ -1,20 +1,18 @@
 
+import java.awt.EventQueue;
 class Main {
   public static void main(String[] args) {
     Cola i = new Cola();
-    //colocar boton que devuelva bolean y colocar en l<=4
-    for(int l=1; l<=4; l++){
+    for(int l=1; l<6 ; l++){//frame.Getstart()
       i.Insert(l);
-    }
-    System.out.println("Nodo ingresado");
+   }
+    //System.out.println("Nodo ingresado");
     i.DeployCola();
     //i.Serch();
     //i.Change();
     //System.out.println("Nodo change");
     //i.DeployCola();
-    i.Delete();
-    System.out.println("Nodo delete");
-    i.DeployCola();
-    
+    //i.Delete();
+    //i.DeployCola();
   }
 }
