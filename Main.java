84706f18460f27fd171e90696e60ee160f4cc70d
LAURA CATALINA PRECIADO BALLEN <92498586+kt6delta@users.noreply.github.com>
2022-07-10 18:08:50 +0000

    4base

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
 
 class Main {
   public static void main(String[] args) {
-    Cola i = new Cola();
-    //colocar boton que devuelva bolean y colocar en l<=4
-    for(int l=1; l<=4; l++){
-      i.Insert(l);
+    Cola c = new Cola();
+    
+    for(int l=1; l<4; l++){
+      c.Insert(l);
     }
-    System.out.println("Nodo ingresado");
-    i.DeployCola();
+    //System.out.println("Nodo ingresado");
+    c.DeployCola();
+     for(int i=1; true; i++){
+       int num = (int)(Math.random()*4+2); //2-4
+       for(int l=1; l<num; l++){
+        c.Insert(l);
+      }
+    }
     //i.Serch();
     //i.Change();
     //System.out.println("Nodo change");
